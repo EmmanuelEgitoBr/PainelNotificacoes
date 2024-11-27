32670b3ec95fbf0b4a3701da1f00f48c8df7d512
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2024-11-27 20:43:10 -0300

    Criando a estrutura do projeto

---
+++ RealTimeNotificationApp/Painel/RealTimeNotificationApp.Painel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealTimeNotificationApp.Painel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
